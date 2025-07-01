0f7b7b168e0c1ec684818ee394101f7c051d0009
sachin jha <sahilrodies000@gmail.com>
2025-07-01 17:01:43 +0530

    tiny fixes

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { getPosts, getPost, getUserPosts ,createPost,likePost,deletePost} from "../controllers/post.controller.js";
 const router=express.Router();
 import { protectRoute } from "../middleware/auth.middleware.js";
+import upload from "../middleware/upload.middleware.js";
 //Public routes
 router.get("/",getPosts)
 router.get("/:postId",getPost)
